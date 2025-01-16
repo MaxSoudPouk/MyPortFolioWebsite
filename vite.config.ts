e80f2cf8a71a38404b25cc6594d8db3b53c0c53d
maxsoudpouk <maxsoudpouk@gmail.com>
2025-01-16 20:21:10 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Set this to '/' or your subdirectory if applicable
+  build: {
+    outDir: 'dist', // Make sure this matches your Vercel deployment setup
+  },
 });
+
