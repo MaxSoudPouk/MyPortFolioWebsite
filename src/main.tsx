f1356fa896c9124165e9ac1e34e445db7d4b2b8e
maxsoudpouk <maxsoudpouk@gmail.com>
2025-01-16 20:11:11 +0700

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <SpeedInsights />
   </React.StrictMode>,
 )
